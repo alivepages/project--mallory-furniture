7302c0ed81207282bd5a3adc0d5dd160b7055be9
Joel <joel.fagundo@gmail.com>
2017-11-06 01:06:56 -0600

    explorer mode

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,20 +4,26 @@ import logo from '../images/mf-logo-white.svg';
 import {categories} from '../data/categories';
 
 export default class Nav extends Component {
+  checkLocation = (route, def = '') => {
+    if (window.location.hash === '#' + route) {
+      return def + ' active';
+    }
+    return def;
+  }
   render() {
     var links = categories.map((category, index) => {
        var url = '/category/' + category;
-       return <Link to={url} key={index} className="category">{category}</Link>
+       return <Link to={url} key={index} className={this.checkLocation(url, 'category')}>{category}</Link>
     })
     return (
       <nav>
         <div>
           <Link to="/"><img className="logo" src={logo} alt="logo"/></Link>
           <div>
-            <Link to="/about">About</Link>
-            <Link to="/terms">Terms+Conditions</Link>
+            <Link to="/about" className={this.checkLocation('/about')}>About</Link>
+            <Link to="/terms" className={this.checkLocation('/terms')}>Terms+Conditions</Link>
             <span className="separator">|</span>
-            <Link to="/all-products">All</Link>
+            <Link to="/all-products" className={this.checkLocation('/all-products')}>All</Link>
             {links}
             <span className="separator">|</span>
             <i className="fa fa-shopping-cart cart" aria-hidden="true" onClick={this.props.cartOpen}></i>
